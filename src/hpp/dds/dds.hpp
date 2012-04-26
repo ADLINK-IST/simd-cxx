ab68c597e8ff741c80114c3fc31a060a901e615a
Angelo Corsaro <angelo@icorsaro.net>
2012-04-26 23:18:08 +0200

    Added top-level include for DDS API

---
+++ src/hpp/dds/dds.hpp
@@ -0,0 +1,13 @@
+#ifndef __OMG_DDS_DDS_HPP__
+#define __OMG_DDS_DDS_HPP__
+
+#include <dds/domain/DomainParticipant.hpp>
+#include <dds/pub/Publisher.hpp>
+#include <dds/topic/TopicTraits.hpp>
+#include <dds/topic/Topic.hpp>
+#include <dds/pub/DataWriter.hpp>
+#include <dds/sub/sub.hpp>
+#include <org/opensplice/topic/TopicTraits.hpp>
+
+
+#endif /* __OMG_DDS_DDS_HPP__ */
