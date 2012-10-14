a46bfe6036a389f0f024f87935033e3dd0ef709e
Angelo Corsaro <angelo@icorsaro.net>
2012-10-14 23:23:48 +0200

    Added top level include for domain module

---
+++ src/hpp/dds/domain/ddsdomain.hpp
@@ -0,0 +1,7 @@
+#ifndef OMG_DDS_DOMAIN_PACKAGE_INCLUDE_HPP_
+#define OMG_DDS_DOMAIN_PACKAGE_INCLUDE_HPP_
+
+#include <dds/domain/find.hpp>
+#include <dds/domain/discovery.hpp>
+
+#endif /* OMG_DDS_DOMAIN_PACKAGE_INCLUDE_HPP_ */
